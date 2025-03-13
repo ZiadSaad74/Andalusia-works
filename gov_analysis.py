6c145c594c12be1ca1acbf10e6fea8028e0c67a7
Ziad M Saad <73083464+ZiadSaad74@users.noreply.github.com>
2025-03-13 13:26:55 +0200

    Update gov_analysis.py

--- gov_analysis.py
+++ gov_analysis.py
@@ -49,7 +49,7 @@ result_counts = df_filtered['Last Reached Call Result'].value_counts().reset_ind
 result_counts.columns = ['Result', 'Count']
 print(counts)
 
-st.subheader(f'Governorates results analysis \n Location : {location_selection} \n Agent : {agents_list} ')
+st.subheader(f'Governorates results analysis \n Location : {location_selection} <br> Agent : {agents_list} ')
 
 if not result_counts.empty:
     fig1 = px.bar(result_counts, x='Result', y='Count', color='Result', title= "Distribution of Calls Results")
