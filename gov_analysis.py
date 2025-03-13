375b0191fab524b5b3e400423f5e0ce4739add16
Ziad M Saad <73083464+ZiadSaad74@users.noreply.github.com>
2025-03-13 13:09:19 +0200

    Update gov_analysis.py

--- gov_analysis.py
+++ gov_analysis.py
@@ -25,11 +25,11 @@ end_date = st.sidebar.date_input("End Date", max_date, min_value=min_date, max_v
 
 counts = list(df.location.unique())
 counts.insert(0,'All locations')
-location_selection = st.sidebar.selectbox("Select location", counts )
+location_selection = st.sidebar.multiselect("Select location", counts )
 
 agents = list(df.Owner.unique())
 agents.insert(0,"All agents")
-agents_list  = st.sidebar.selectbox("Agent",agents)
+agents_list  = st.sidebar.multiselect("Agent",agents)
 
 if location_selection !='All locations':
     df_filtered = df[df['location']==location_selection]
