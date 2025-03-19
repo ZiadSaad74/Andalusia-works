90337c2cdf1cd99c99e3ebb0d3fd103d146f69cb
Ziad M Saad <73083464+ZiadSaad74@users.noreply.github.com>
2025-03-19 11:17:08 +0200

    Update gov_analysis.py

--- gov_analysis.py
+++ gov_analysis.py
@@ -23,7 +23,9 @@ default_start_date = max(min_date, max_date - timedelta(days=365))
 start_date = st.sidebar.date_input("Start Date", default_start_date, min_value=min_date, max_value=max_date)
 end_date = st.sidebar.date_input("End Date", max_date, min_value=min_date, max_value=max_date)
 
-counts = list(df['Last Reached Call Result'].unique())
+counts = ['Interested','Interested but not available','Interested Follow Up Required','Booked']
+df = df[df['Last Reached Call Result'].isin(counts)]
+
 counts.sort()
 counts.insert(0,'All results')
 result_selection = st.sidebar.selectbox("Select result", counts )
@@ -38,6 +40,7 @@ agents.sort()
 agents.insert(0,"All agents")
 agents_list  = st.sidebar.selectbox("Agent",agents)
 
+
 df_filtered = df[(df['Created On'] >= pd.Timestamp(start_date)) & (df['Created On'] <= pd.Timestamp(end_date))]
 
 if result_selection !='All results':
@@ -49,6 +52,10 @@ if agents_list !='All agents':
 if locations !='All locations':
     df_filtered = df_filtered[df_filtered['location']==locations]
 
+
+locations_counts = df_filtered.location.value_counts().reset_index()
+locations_counts.columns = ['Location','Count']
+
 result_counts = df_filtered['location'].value_counts().reset_index()
 result_counts.columns = ['Location', 'Count']
 
@@ -59,8 +66,8 @@ st.subheader(f'Governorates results analysis')
 st.markdown(f'Result : <b>{result_selection}</b> <br> Agent : <b>{agents_list}</b><br> Total leads = <b>{len(df_filtered)}</b><br> Location : <b>{locations}</b>', unsafe_allow_html=True)
 
 if not result_counts.empty:
-    fig1 = px.bar(result_counts, x='Location', y='Count', color='Location', title= "Distribution of locations by result")
-    fig2 = px.pie(result_counts, names='Location', values='Count', title= "Perecentge of each location")
+    fig1 = px.bar(locations_counts, y='Location', x='Count', color='Location',orientation='h', title= "Distribution of most interested results VS location <br> [Interested - Interested but not available - Interested Follow Up Required - Booked]")
+    fig2 = px.pie(locations_counts, names='Location', values='Count', title= "Perecentge of each location")
     fig3 = px.bar(result_counts2, x="Result", y= 'Count', title='Distribution of results')
 
     st.plotly_chart(fig1, use_container_width=True)
